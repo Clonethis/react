8b804daa34086fa3d091822996219f9fecbafc6c
Jiří Brtník <jirka.brtnik@post.cz>
2021-07-05 21:43:09 +0200

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/index.js 	new file:   src/tnoeuhns.html

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function App(){
+    return(
+        <div>           
+            <h1>Hello</h1>
+                <img src="https://cdn.myshoptet.com/tpl/81bcd898/shoptet_responsive/master/templates/frontend_templates/shoptet_responsive/img/whyUs/general/8_automatizace.png"/>
+        </div>
+    );
+}
+
+
+ReactDOM.render(App(),document.getElementById('root'));
+
